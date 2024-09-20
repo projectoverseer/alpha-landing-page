54938674e601adfe9cbd2a981f96736d16e9ec49
projectoverseer <banz04@icloud.com>
2024-09-20 14:49:37 +0700

    Upgrade to Bootstrap 4.4.1

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+const navbar = document.querySelector("#mainNav");
+window.onscroll = () => {
+  if (window.scrollY > 0) {
+    navbar.classList.add("nav-active");
+  } else {
+    navbar.classList.remove("nav-active");
+  }
+};
