6fa23e078d9552bc4cd4488228a87f86ef289510
projectoverseer <banz04@icloud.com>
2024-09-21 23:22:31 +0700

    Update color scheme and web appearance

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,6 @@
 const navbar = document.querySelector("#mainNav");
 window.onscroll = () => {
-  if (window.scrollY > 0) {
+  if (window.scrollY > 2) {
     navbar.classList.add("nav-active");
   } else {
     navbar.classList.remove("nav-active");
