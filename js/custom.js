cdd4477b722d74287820cabcd276d16ec0adece9
projectoverseer <banz04@icloud.com>
2024-10-02 21:04:32 +0700

    Simplify HTML code

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,16 @@
-const navbar = document.querySelector("#mainNav");
+const navbar = document.querySelector('#mainNav');
 window.onscroll = () => {
-  if (window.scrollY > 2) {
-    navbar.classList.add("nav-active");
+  if (window.scrollY > 3) {
+    navbar.classList.add('nav-active')
   } else {
-    navbar.classList.remove("nav-active");
+    navbar.classList.remove('nav-active')
   }
 };
+
+function removeHash() { 
+  history.pushState('', document.title, window.location.pathname + window.location.search)
+}
+
+addEventListener("popstate", (event) => {
+  removeHash()
+});
