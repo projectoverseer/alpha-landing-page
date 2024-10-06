089a905be50d52c0fcf4ee30c0b0b5ece17b7e7d
projectoverseer <banz04@icloud.com>
2024-10-06 14:19:40 +0700

    Adjust spacing and type scale

--- js/custom.js
+++ js/custom.js
@@ -1,16 +1,8 @@
 const navbar = document.querySelector('#mainNav');
 window.onscroll = () => {
-  if (window.scrollY > 3) {
+  if (window.scrollY > 0) {
     navbar.classList.add('nav-active')
   } else {
     navbar.classList.remove('nav-active')
   }
 };
-
-function removeHash() { 
-  history.pushState('', document.title, window.location.pathname + window.location.search)
-}
-
-addEventListener("popstate", (event) => {
-  removeHash()
-});
