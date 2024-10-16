d250de3715713ff297e7428da2eedfe6ba1488a5
projectoverseer <banz04@icloud.com>
2024-10-16 14:51:12 +0700

    Slightly improve typography and performance

--- js/custom.js
+++ js/custom.js
@@ -1,48 +1,31 @@
-(function() {
+(function () {
   function updateNavState() {
     const navbar = document.querySelector('#globalnav');
     if (window.scrollY > 0) {
       navbar.classList.add('nav-active');
       navbar.setAttribute('data-bs-theme', 'light');
     } else {
+      const navbar = document.querySelector('#globalnav');
       navbar.classList.remove('nav-active');
       navbar.setAttribute('data-bs-theme', 'dark');
     }
   }
 
-  // Debounce function to limit how often updateNavState is called
-  function debounce(func, wait) {
-    let timeout;
-    return function executedFunction(...args) {
-      const later = () => {
-        clearTimeout(timeout);
-        func(...args);
-      };
-      clearTimeout(timeout);
-      if (window.scrollY < 120) {
-        timeout = setTimeout(later, wait);
-      } else {
-        timeout = setTimeout(later, 0);
-      }
-    };
-  }
-
   updateNavState(); // updates nav state on page load
-  window.addEventListener("scroll", debounce(updateNavState, 100));
+  window.addEventListener('scroll', updateNavState);
 
-  // Prevents hash from changing
+  // Updates URL to remove hash
   function removeHash() {
-    if (window.location.hash) {
-      history.pushState('', document.title, window.location.pathname + window.location.search);
-    }
+    history.replaceState('', document.title, window.location.pathname
+      + window.location.search);
   }
 
-  window.addEventListener("hashchange", removeHash, false);
+  window.addEventListener('hashchange', removeHash, false);
 
   // Removes queries on page load
   if (location.search) {
     const cleanUrl = location.href.split('?')[0];
-    history.replaceState(null, '', cleanUrl); // Use replaceState instead of pushState
+    history.replaceState(null, '', cleanUrl);
   }
 
   removeHash();
