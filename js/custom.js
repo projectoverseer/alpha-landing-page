faa7ff9248744223bfd4f754f756faeea359d079
projectoverseer <banz04@icloud.com>
2024-10-16 09:31:14 +0700

    Use Apple system font for Inter; prevent hash from updating; and other minor user experience improvements

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,28 @@
-const navbar = document.querySelector('#globalnav');
+const navbar = document.querySelector('#globalnav')
 window.onscroll = () => {
   if (window.scrollY > 0) {
     navbar.classList.add('nav-active')
+    navbar.setAttribute('data-bs-theme', 'light')
   } else {
     navbar.classList.remove('nav-active')
+    navbar.setAttribute('data-bs-theme', 'dark')
   }
-};
+}
+
+
+// Prevents hash from changing
+function removeHash() {
+  history.pushState('', document.title, window.location.pathname
+    + window.location.search);
+}
+
+window.addEventListener(
+  "hashchange",
+  () => {
+    removeHash()
+  }, false
+)
+
+// Removes queries on page load
+history.pushState(null, '', location.href.split('?')[0])
+removeHash()
