9cb91619234915df8bf577418c36af07adb7db86
projectoverseer <banz04@icloud.com>
2025-02-09 18:09:36 +0700

    Simplified relationships among design elements and slightly modified the grayscale color scheme

--- js/custom.js
+++ js/custom.js
@@ -1,30 +1,34 @@
 (function () {
-  const navbar = document.querySelector('#globalnav');
+  const navbar = document.querySelector("#globalnav");
 
   function updateNavState() {
     if (window.scrollY > 12) {
-      navbar.classList.add('nav-active');
-      navbar.setAttribute('data-bs-theme', 'light');
+      navbar.classList.add("nav-active");
+      navbar.setAttribute("data-bs-theme", "light");
     } else {
-      navbar.classList.remove('nav-active');
-      navbar.setAttribute('data-bs-theme', 'dark');
+      navbar.classList.remove("nav-active");
+      navbar.setAttribute("data-bs-theme", "dark");
     }
   }
 
   function removeHash() {
-    history.replaceState('', document.title, window.location.pathname + window.location.search);
+    history.replaceState(
+      "",
+      document.title,
+      window.location.pathname + window.location.search
+    );
   }
 
   function cleanUrl() {
     if (location.search) {
-      const cleanUrl = location.href.split('?')[0];
-      history.replaceState(null, '', cleanUrl);
+      const cleanUrl = location.href.split("?")[0];
+      history.replaceState(null, "", cleanUrl);
     }
   }
 
-  updateNavState(); // updates nav state on page load
-  window.addEventListener('scroll', updateNavState);
-  window.addEventListener('hashchange', removeHash, false);
+  updateNavState(); // update navbar upon page load
+  window.addEventListener("scroll", updateNavState);
+  window.addEventListener("hashchange", removeHash, false);
 
   cleanUrl();
   removeHash();
