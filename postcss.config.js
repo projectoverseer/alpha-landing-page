2b862d5c03d0a4b9a5183c38da99bca1c0f91116
projectoverseer <banz04@icloud.com>
2025-05-13 11:32:01 +0700

    Updated to Bootstrap 5.3.6; used a more systematic typography choice; updated icons and content

--- postcss.config.js
+++ postcss.config.js
@@ -1,17 +1,27 @@
-const fs = require('fs');
+const fs = require("fs");
 
 function saveMap(outputMap) {
-    // Save the output map to a JSON file
-    fs.writeFileSync('_classMap.json', JSON.stringify(outputMap, null, 2));
+  // Save the output map to a JSON file
+  fs.writeFileSync("_classMap.json", JSON.stringify(outputMap, null, 2));
 }
 
 module.exports = {
-    plugins: {
-        'postcss-rename': {
-            strategy: 'minimal',
-            by: 'whole',
-            except: ['active'],
-            outputMapCallback: saveMap,
-        },
+  plugins: {
+    "postcss-rename": {
+      strategy: "minimal",
+      by: "whole",
+      except: [
+        "active",
+        "show",
+        "collapsed",
+        "collapse",
+        "transition",
+        "collapsing",
+        "overflow-hidden",
+        "height",
+        "position-relative",
+      ],
+      outputMapCallback: saveMap,
     },
+  },
 };
